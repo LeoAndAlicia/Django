5256c4503f41e4946f4524da01f4d8a690541aec
LeoAndAlicia <355186603@qq.com>
2019-03-20 16:49:20 +0800

    update

--- mydjango/blog/urls.py
+++ mydjango/blog/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^index/', views.index, name='index'),
     url(r'^page/(?P<article_id>[0-9]+)', views.article_page, name='article_page'),
-    url(r'^edit/', views.edit, name='edit'),
+    url(r'^edit/(?P<article_id>[0-9]+)', views.edit, name='edit'),
     url(r'^action/', views.edit_action, name='edit_action'),
 ]
